17c71b599fa63e9202aa599c64726951ad6917ac
Mr-DooSun <mrdoosun@gmail.com>
2024-07-27 05:32:01 +0900

    Add : proccess focus

--- event.py
+++ event.py
@@ -1,36 +1,59 @@
-from pywinauto import findwindows, findbestmatch
-from pywinauto import application
+from pywinauto import findwindows, application, ElementNotFoundError
+from pywinauto.application import WindowSpecification
 
-from typing import Optional
+from literal.stove import STOVE_CLIENT_TITLE, STOVE_CLIENT_CLASS
 
-STOVE_CLIENT = "STOVE"
+
+def exists_stove_client() -> bool:
+    try:
+        findwindows.find_element(title=STOVE_CLIENT_TITLE, class_name=STOVE_CLIENT_CLASS)
+        return True
+    except ElementNotFoundError:
+        return False
+
+
+def get_stove_client_process_id() -> int:
+    proc = findwindows.find_element(title=STOVE_CLIENT_TITLE, class_name=STOVE_CLIENT_CLASS)
+    return proc.process_id
+
+
+def focus_stove_client(stove_id: int):
+    application.Application(backend="win32").connect(process=stove_id)[STOVE_CLIENT_TITLE].set_focus()
+
+
+def logout_event(stove_client: WindowSpecification):
+    stove_client["CheckBox6"].wrapper_object().click()
+    stove_client["로그아웃"].click()
+    stove_client["로그아웃 하기"].click()
     
-def get_stove_client_process_id() -> Optional[int]:
-    procs = findwindows.find_elements()
+def game_start_event(stove_client: WindowSpecification):
+    stove_client["게임 시작"].click()
     
-    proc_id = None
-    for proc in procs:
-        if proc.name == STOVE_CLIENT:
-            proc_id = proc.process_id
-            break
+def login_event(stove_client: WindowSpecification, id:str, pw:str):
+    pass
     
-    return proc_id
 
-def get_processes() -> Optional[int]:
-    procs = findwindows.find_elements()
 
-    for proc in procs:
-        print(proc, proc.process_id)
+def main():
+    if not(exists_stove_client()):
+        print("open stove")
 
-if __name__ == "__main__" :
-    app = application.Application(backend='uia')
     stove_id = get_stove_client_process_id()
-    
-    app.connect(process=stove_id)
-    stove = app["STOVE"]
-    print(stove.print_control_identifiers())
-    
 
-    # app.connect(title_re=STOVE_CLIENT)
+    focus_stove_client(stove_id=stove_id)
+
+    app = application.Application(backend="uia").connect(process=stove_id)
+    stove_client = app["STOVE"]
+
+    logout_event(stove_client=stove_client)
+
+
+if __name__ == "__main__":
+    # main()
     
-    # print(app)
+    stove_id = get_stove_client_process_id()
+    app = application.Application(backend="uia").connect(process=stove_id)
+    stove_client = app["STOVE"]
+    print(stove_client.print_control_identifiers())
+
+
